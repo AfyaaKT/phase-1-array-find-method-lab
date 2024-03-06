224737702184a1ac7b8a39cfa569a8903870ccc9
unknown <afyaakhudur@gmail.com>
2024-03-07 02:31:12 +0300

    complete assynment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" },
+  //...
+];
+function superbowlWin(arr) {
+    
+  const obj = arr.find((element) => element.result === "W");
+ return obj ? obj.year : undefined
+  
+}
+const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+
+console.log(superbowlWin(record));
+console.log(superbowlWin(sadReality));
+
